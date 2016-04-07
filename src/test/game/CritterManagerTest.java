e83e51bb25dae38c3c8daeb67dd77c31d5f2df0a
Sahidul Islam <sahidul.net@gmail.com>
2016-04-06 21:48:40 -0400

    Update critter manager test

--- src/test/game/CritterManagerTest.java
+++ src/test/game/CritterManagerTest.java
@@ -25,7 +25,7 @@ public class CritterManagerTest {
     @Rule
     public JavaFXThreadingRule javafxRule = new JavaFXThreadingRule();
     CritterManager leCritterManager;
-    private Tile[][] leTiles = new Tile[2][3];
+    private Tile[][] leTiles = new Tile[6][6];
     private PathFinder lePath;
     private ArrayList<Critter> leCritters;
     private Tower leTower;
@@ -36,21 +36,24 @@ public class CritterManagerTest {
         leTiles[0][0] = new Tile(SpriteType.ENTRY_POINT,32,32,new Vector2(0,0));
         leTiles[0][1] = new Tile(SpriteType.PATH,32,32,new Vector2(1,0));
         leTiles[0][2] = new Tile(SpriteType.PATH,32,32,new Vector2(2,0));
-        leTiles[0][0] = new Tile(SpriteType.PATH, 32,32,new Vector2(3,0));
-        leTiles[0][0] = new Tile(SpriteType.PATH, 32,32,new Vector2(3,0));
-        leTiles[0][2] = new Tile(SpriteType.EXIT_POINT, 32, 32, new Vector2(4,0));
-        lePath = new PathFinder(leTiles,3,2);
+        leTiles[0][3] = new Tile(SpriteType.PATH, 32,32,new Vector2(3,0));
+        leTiles[0][4] = new Tile(SpriteType.PATH, 32,32,new Vector2(3,0));
+        leTiles[0][5] = new Tile(SpriteType.EXIT_POINT, 32, 32, new Vector2(4,0));
+
+        lePath = new PathFinder(leTiles, 6, 6);
         leCritterManager = new CritterManager(lePath);
-        leCritters.add(new Critter(new Vector2(0,0), CritterType.GROUND));
-        leCritters.add(new Critter(new Vector2(1,0), CritterType.GROUND));
+        leCritters = new ArrayList<>();
+        leCritters.add(new Critter(new Vector2(0,0), CritterType.AIR));
+        leCritters.add(new Critter(new Vector2(1,0), CritterType.AIR));
         leCritterManager.SetCritterList(leCritters);
+
         leTower = new Tower(TowerType.ARROW, new Vector2(1,2));
     }
 
-    /*@Test
+    @Test
     public void getShootableCrittersTest()
     {
         ArrayList<Critter> returnList = leCritterManager.getShootableCritters(leTower);
         Assert.assertTrue(returnList.size() > 0);
-    }*/
+    }
 }
